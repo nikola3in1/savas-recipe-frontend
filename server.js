f966c0e33fadc228428dec4a68ec68eaee4eb675
nikola3in1 <nikola.3in1@hotmail.com>
2018-09-30 22:57:54 +0200

    caching problem

--- server.js
+++ server.js
@@ -4,27 +4,13 @@ const path = require('path');
 
 const app = express();
 
-
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist/savaApp/'));
 
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-
-  https.get('http://savas-recipe-backend.herokuapp.com/start', (resp) => {
-    let data = '';
-  
-    // The whole response has been received. Print out the result.
-    resp.on('end', () => {
-      console.log(JSON.parse(data));
-      res.sendFile(path.join(__dirname+ '/src/index.html'));
-    });
-  
-  }).on("error", (err) => {
-    console.log("Error: " + err.message);
-  });
-
+  res.sendFile(path.join(__dirname+ '/src/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
